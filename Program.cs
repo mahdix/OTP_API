c9bad0d8886371a78537dcc89811a21256126dc4
Mahdi Mohammadi <mm@binary.com>
2016-11-10 07:38:06 +0800

    gitignore file

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 
 namespace ConsoleApplication
 {
@@ -6,7 +10,26 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+			var host = new WebHostBuilder()
+				.UseKestrel()
+				.UseStartup<Startup>()
+				.Build();
+
+			host.Run();
         }
     }
+
+	public class Startup
+	{
+		public void Configure(IApplicationBuilder app)
+		{            
+			app.Run(async (context) =>
+					{
+					await context.Response.WriteAsync(
+							"Hello World. The Time is: " + 
+							DateTime.Now.ToString("hh:mm:ss tt"));
+
+					});
+		}
+	}
 }
