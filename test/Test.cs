71ce284d6b1184f02beebaddcba5a3767e7f676c
Mahdi Mohammadi <mm@binary.com>
2016-11-10 11:16:00 +0800

    nunit enabled - adding some tests (wip)

--- test/Test.cs
+++ test/Test.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using NUnit.Framework;
 using OTP_API;
+using OTP_API.Common;
 
 namespace OTP_API.Test
 {
@@ -10,6 +11,22 @@ namespace OTP_API.Test
     {
         [TestCase("mahdi")]
         public void TestProcess(string user)
+        {
+            Assert.AreEqual("mahdi", user);
+        }
+
+        [TestCase]
+        public void TestStorage()
+        {
+        }
+
+        [TestCase]
+        public void TestLogin()
+        {
+        }
+
+        [TestCase]
+        public void TestRegister()
         {
         }
 
