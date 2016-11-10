22d46b309ef75881a5bc7ba5cbce601ce6b2e137
Mahdi Mohammadi <mm@binary.com>
2016-11-10 10:50:42 +0800

    generating hash-based password during registration

--- Registeration.cs
+++ Registeration.cs
@@ -1,6 +1,8 @@
 using System;
 using System.IO;
+using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -11,9 +13,24 @@ namespace OTP_API
     {
         public static Task Process(HttpContext context) {
             string userName = context.Request.Query["user"]; 
-            Storage.Set(userName, "pass");
+            string otp = GenerateOneTimePassword(userName);
+            Storage.Set(userName, otp);
 
-            return context.Response.WriteAsync("pass");
+            return context.Response.WriteAsync(otp);
+        }
+
+        private static string GenerateOneTimePassword(string user) 
+        {
+            return Hash(user);
+        }
+
+        private static string Hash(string password)
+        {
+            using (var sha = SHA256.Create())
+            {
+                var computedHash = sha.ComputeHash(Encoding.ASCII.GetBytes(password));
+                return BitConverter.ToString(computedHash).Replace("-","");
+            }
         }
     }
 }
